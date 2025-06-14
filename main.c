790480bd61c888a2150d3d558b5556f8144b2889
hailey-royle <hailey.royle@proton.me>
2025-06-13 21:54:34 -0400

    added atom trace

--- main.c
+++ main.c
@@ -6,17 +6,29 @@
 #define FPS 60
 #define HALF_SPRITE_WH 32
 #define GRAVITY 0.25
+#define ATOM_TRACE_THICK 4
 
 #define GRAV_BLACK (Color){15, 15, 15, 255}
 #define GRAV_WHITE (Color){239, 239, 239, 255}
+#define GRAV_DGRAY (Color){63, 63, 63, 255}
 
 Texture2D _testingGraviton;
 Texture2D _testingAtom;
+
+int currentAtomTraceSection = 0;
 
 Vector2 gravitonPosition = (Vector2){(WINDOW_WIDTH / 2), (WINDOW_HIGHT / 2)};
 Vector2 atomPosition = (Vector2){255, 255};
 Vector2 atomSpeed = (Vector2){0, 0};
 Vector2 atomForce = (Vector2){0, 0};
+
+struct AtomTraceSection {
+    Vector2 start;
+    Vector2 end;
+    bool active;
+};
+
+struct AtomTraceSection atomTrace[FPS];
 
 void UpdateGraviton() {
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
@@ -24,10 +36,23 @@ void UpdateGraviton() {
     }
 }
 
+void UpdateAtomTrace() {
+    atomTrace[currentAtomTraceSection].start = atomPosition;
+    atomTrace[currentAtomTraceSection].end = Vector2Add(atomPosition, atomSpeed);
+    atomTrace[currentAtomTraceSection].active = true;
+    if (currentAtomTraceSection < FPS) {
+        ++currentAtomTraceSection;
+    } else {
+        currentAtomTraceSection = 0;
+    }
+}
+
+
 void UpdateAtom() {
     atomForce = Vector2Scale(Vector2Subtract(gravitonPosition, atomPosition), (GRAVITY / Vector2Distance(atomPosition, gravitonPosition)));
     atomSpeed = Vector2Add(atomSpeed, atomForce);
     atomPosition = Vector2Add(atomPosition, atomSpeed);
+    UpdateAtomTrace();
 }
 
 void Update() {
@@ -35,19 +60,29 @@ void Update() {
     UpdateAtom();
 }
 
-void DrawGraviton() {
-    DrawTextureV(_testingGraviton, Vector2SubtractValue(gravitonPosition, HALF_SPRITE_WH), GRAV_WHITE);
+void DrawAtomTrace() {
+    int i = 0;
+    for (i = 0; i <= FPS; i += 1) {
+         if ((atomTrace[i].active = true)) {
+            DrawLineEx(atomTrace[i].start, atomTrace[i].end, ATOM_TRACE_THICK, GRAV_DGRAY);
+         }
+    }
 }
 
 void DrawAtom() {
+    DrawAtomTrace();
     DrawTextureV(_testingAtom, Vector2SubtractValue(atomPosition, HALF_SPRITE_WH), GRAV_WHITE);
+}
+
+void DrawGraviton() {
+    DrawTextureV(_testingGraviton, Vector2SubtractValue(gravitonPosition, HALF_SPRITE_WH), GRAV_WHITE);
 }
 
 void Draw() {
     BeginDrawing();
         ClearBackground(GRAV_BLACK);
-        DrawGraviton();
         DrawAtom();
+        DrawGraviton();
     EndDrawing();
 }
 
